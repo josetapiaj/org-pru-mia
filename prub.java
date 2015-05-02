520641403d67b25079f007e8541cfe3d112d5a06
Jose Tapia <jose@localhost.localdomain>
2015-05-02 17:21:44 -0300

    [issue3] tercero comiyaciion ddd

--- prub.java
+++ prub.java
@@ -2,9 +2,8 @@ public class prub{
 	public static void main(String[] args){
 
 		System.out.println("hola pibe");
-		System.out.println("Hola pendejos");
-		System.out.println("CACACA CACCCCA");
-}
+		
 
+}
 }
 
