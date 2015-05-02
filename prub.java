5a945e10bcf53a02ed366187a8d37a24a49846d0
Jose Tapia <jose@localhost.localdomain>
2015-05-02 12:03:41 -0300

    mi segund comitoasad asddas

--- prub.java
+++ prub.java
@@ -1,7 +1,9 @@
-public class pru{
-	public static void main(string[] args){
+public class prub{
+	public static void main(String[] args){
 
 		System.out.println("hola pibe");
+		System.out.println("Hola pendejos");
+		System.out.println("CACACA CACCCCA");
 }
 
 }
